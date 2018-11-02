f9a415b9453680d4ea3d5eafc2714d13e6225c16
yue.wang <wyue.leon@gmail.com>
2018-11-02 23:34:41 +0100

    Modidy variable name using snake_case

--- leap/leap.py
+++ leap/leap.py
@@ -6,11 +6,7 @@
 # y	y	y	y
 
 def is_leap_year(year):
-    isLeapYear = True
-
-    if year%4 != 0 or (year%100 == 0 and year%400 !=0):
-        isLeapYear=False
-    return isLeapYear
+    return not(year%4 != 0 or (year%100 == 0 and year%400 !=0))
 
 
 
