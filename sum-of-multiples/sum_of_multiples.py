c68d5c2ea2aeda17ee8637839787f177c3f351b4
yue.wang <wyue.leon@gmail.com>
2018-11-15 21:20:11 +0100

    Refactoring

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,5 @@
+def has_any_multiple(number, multiples):
+    return any(number % m == 0 for m in multiples)
+
 def sum_of_multiples(limit, multiples):
-    return sum(n for n in range(1, limit) if any(n % m == 0 for m in multiples))
+    return sum(n for n in range(1, limit) if has_any_multiple(n, multiples))
