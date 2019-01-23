3c4f59079539ce6b4ce7ec35d765b1cb56471b83
yue.wang <wyue.leon@gmail.com>
2019-01-23 21:28:41 +0100

    Refactorying

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,12 +1,11 @@
 def raindrops(number):
-    if number % 3 != 0 and number % 5 != 0 and number % 7 != 0:
-        return str(number)
-    else:
-        res = ""
-        if number % 3 == 0:
-            res += 'Pling'
-        if number % 5 == 0:
-            res += 'Plang'
-        if number % 7 == 0:
-            res += 'Plong'
-        return res
+    res = ""
+    if number % 3 == 0:
+        res += 'Pling'
+    if number % 5 == 0:
+        res += 'Plang'
+    if number % 7 == 0:
+        res += 'Plong'
+    if len(res) == 0:
+        res = str(number)
+    return res
